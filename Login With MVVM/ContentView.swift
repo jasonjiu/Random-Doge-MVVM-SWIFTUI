c9697c469c066a8b942e75bf1effbc90df45559b
jason <jasonanggarah@gmail.com>
2021-04-19 15:06:19 +0700

    Initial Commit

---
+++ Login With MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login With MVVM
+//
+//  Created by Jason Prosia on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login With MVVM/Login_With_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_With_MVVMApp.swift
+//  Login With MVVM
+//
+//  Created by Jason Prosia on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Login_With_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
